0ea5f5088d01831b02398c00d55bb2c343f8c191
n0RE <nore@desktop>
2023-04-07 15:07:32 +0300

    added loginmodal

--- twitter-clone/pages/_app.tsx
+++ twitter-clone/pages/_app.tsx
@@ -3,10 +3,14 @@ import Layout from '@/components/Layout'
 import store from '@/store/store'
 import { Provider } from 'react-redux'
 import '@/styles/globals.css'
+import Modal from '@/components/Modal'
+import LoginModal from '@/components/modals/LoginModal'
 
 export default function App({ Component, pageProps }: AppProps) {
+
   return (
     <Provider store={store}>
+      <LoginModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
