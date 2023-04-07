ff154ddead4ba6b5ace8450e4c517561a145aeb0
n0RE <nore@desktop>
2023-04-07 10:54:52 +0300

    added custom Input

---
+++ twitter-clone/components/UI/Input.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface InputProps {
+    placeholder: string
+    value: any
+    type: string
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const Input: React.FC<InputProps> = ({placeholder, value, type, onChange}) => {
+    return (
+        <input placeholder={placeholder} value={value} type={type} onChange={onChange} className='w-full p-4 text-lg bg-black border-2 border-neutral-800 rounded-md outline-none text-white focus:border-2 focus:border-sky-500 transition'/>
+    );
+};
+
+export default Input;
