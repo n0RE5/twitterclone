b9ada7822d021932acdf4fba90f20fa9df735f3a
n0RE <nore@desktop>
2023-04-07 18:14:20 +0300

    added userModal

--- twitter-clone/pages/_app.tsx
+++ twitter-clone/pages/_app.tsx
@@ -5,11 +5,13 @@ import { Provider } from 'react-redux'
 import '@/styles/globals.css'
 import Modal from '@/components/Modal'
 import LoginModal from '@/components/modals/LoginModal'
+import EditUserModal from '@/components/modals/EditUserModal'
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <Provider store={store}>
+      <EditUserModal />
       <LoginModal />
       <Layout>
         <Component {...pageProps} />
