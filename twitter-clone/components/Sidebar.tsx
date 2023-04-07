fc537628de6413b134999f3b55b01bdbcf0b7db9
n0RE <nore@desktop>
2023-04-07 15:05:58 +0300

    added logout function

--- twitter-clone/components/Sidebar.tsx
+++ twitter-clone/components/Sidebar.tsx
@@ -1,25 +1,38 @@
 import { useState } from "react";
-import { BsHouseFill } from 'react-icons/bs'
+import { BsHouseFill, } from 'react-icons/bs'
+import { BiLogOut } from "react-icons/bi";
 import { FaUser } from 'react-icons/fa'
 import SidebarItem from "./UI/SidebarItem";
 import SidebarTweet from "./UI/SidebarTweet";
 import Logo from "./UI/Logo";
 import { useRouter } from "next/router";
+import { useAppDispatch, useAppSelector } from "@/hooks/useRedux";
+import { userLogout } from "@/store/reducers/userSlice";
 
 const Sidebar = () => {
     const router = useRouter()
+    const dispatch = useAppDispatch()
+    const isAuth = useAppSelector(state => state.userSlice.isAuth)
+
+
     const [sidebarItems] = useState([
         {
             title: 'Home',
             href: '/',
-            icon: BsHouseFill
+            icon: BsHouseFill,
         },
         {
             title: 'Profile',
-            href: '/',
+            href: '/users/1',
             icon: FaUser
         },
     ])
+
+    const logout = () => {
+        localStorage.removeItem('token')
+        dispatch(userLogout())
+        //todo custom alert
+    }
 
     return (
         <div className="col-span-1 h-full pr-4 md:pr-4">
@@ -34,6 +47,9 @@ const Sidebar = () => {
                             icon={sidebarItem.icon} 
                         />
                     )}
+                    {isAuth &&
+                        <SidebarItem icon={BiLogOut} title="Logout" onClick={logout} />
+                    }
                     <SidebarTweet />
                 </div>
              </div>
