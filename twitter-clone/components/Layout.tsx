a49b5944d75953468cbafa2aa4f6ba279b01818b
n0RE <nore@desktop>
2023-04-07 15:05:38 +0300

    added check function

--- twitter-clone/components/Layout.tsx
+++ twitter-clone/components/Layout.tsx
@@ -1,11 +1,31 @@
+import { useAppDispatch } from "@/hooks/useRedux"
 import FollowBar from "./FollowBar"
 import Sidebar from "./Sidebar"
+import { fetchUser, fetchUserError, fetchUserSuccess } from "@/store/reducers/userSlice"
+import { check } from "@/httpAPI/authAPI"
+import { useEffect } from "react"
 
 interface LayoutProps {
   children: React.ReactNode
 }
 
 const Layout: React.FC<LayoutProps> = ({children})=> {
+  const dispatch = useAppDispatch()
+
+  const checkUser = async () => {
+    try {
+      dispatch(fetchUser())
+      const response = await check()
+      dispatch(fetchUserSuccess(response))
+    } catch (error: any) {
+        dispatch(fetchUserError(error.response?.data?.message))
+    }
+  }
+
+  useEffect(() => {
+    checkUser()
+  })
+
   return (
     <div className='bg-black h-screen'>
       <div className="container h-full mx-auto xl:px-30 max-w-6xl">
