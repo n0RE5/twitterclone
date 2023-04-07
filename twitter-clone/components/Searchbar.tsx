207d2d43815a031a317deb71db5d5e791dbb10c2
n0RE <nore@desktop>
2023-04-07 21:25:23 +0300

    updated searchbar

--- twitter-clone/components/Searchbar.tsx
+++ twitter-clone/components/Searchbar.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import { useSearchModal } from '@/hooks/useSearchModal';
+import React, { useCallback } from 'react';
 import { FaSearch } from 'react-icons/fa'
 
-interface SearchbarProps {
-    
-}
+const Searchbar = () => {
+    const searchModal = useSearchModal()
 
-const Searchbar: React.FC<SearchbarProps> = () => {
     return (
-        <div className='relative rounded-full w-14 h-14 flex justify-center items-center hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer'>
+        <div onClick={searchModal.openModal} className='relative rounded-full w-14 h-14 flex justify-center items-center hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer'>
             <FaSearch size={28} color='white' />
         </div>
     );
