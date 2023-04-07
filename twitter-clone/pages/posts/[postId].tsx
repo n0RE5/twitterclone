1893e2223510610a3bdb93e27f65b6d0cf355cf5
n0RE <nore@desktop>
2023-04-07 15:05:23 +0300

    updated comment page

--- twitter-clone/pages/posts/[postId].tsx
+++ twitter-clone/pages/posts/[postId].tsx
@@ -1,14 +1,19 @@
 import Header from "@/components/Header";
+import CommentItem from "@/components/posts/CommentItem";
 import PostItem from "@/components/posts/PostItem";
+import { getComments } from "@/httpAPI/commentsAPI";
 import { getPost } from "@/httpAPI/postsAPI";
 import { getById } from "@/httpAPI/userAPI";
 import { InferGetServerSidePropsType } from "next";
 
-const PostPage = ({ user, post }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const PostPage = ({ user, post, comments }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
     return (
         <>
             <Header showArrow title="Tweet" />
-            <PostItem user={user} post={post} />  
+            <PostItem user={user} post={post} />
+            {comments.map(comment =>
+              <CommentItem comment={comment}/>
+            )}  
         </>
     );
 };
@@ -17,10 +22,12 @@ export async function getServerSideProps(context: any) {
     const postId = context.params.postId
     const post = await getPost(postId)
     const user = await getById(post.userId)
+    const comments = await getComments(postId)
     return {
       props: {
         user,
-        post
+        post,
+        comments
       }, 
     }
 }
