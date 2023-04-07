89bb451a0a66e5579393c5e3ff39f26ba2d6c7ef
n0RE <nore@desktop>
2023-04-07 18:14:57 +0300

    added editUserBio function

--- twitter-clone/httpAPI/userAPI.ts
+++ twitter-clone/httpAPI/userAPI.ts
@@ -1,7 +1,13 @@
 import { fetchedUser } from "@/types/Interfaces"
-import { $host } from "./hosts"
+import { $authHost, $host } from "./hosts"
 
 export const getById = async (id: number): Promise<fetchedUser> => {
     const response = await $host.get(`/users/${id}`)
+    return response.data
+}
+
+export const editUserBio = async (dto: any) => {
+    const response = await $authHost.post('users/update', dto)
+    console.log(response);
     return response.data
 }
