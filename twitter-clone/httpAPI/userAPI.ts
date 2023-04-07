37cb301db70c3b20bdbbe47c3cacda0df3cf7bf5
n0RE <nore@desktop>
2023-04-07 21:24:08 +0300

    added searchUser function

--- twitter-clone/httpAPI/userAPI.ts
+++ twitter-clone/httpAPI/userAPI.ts
@@ -1,6 +1,10 @@
 import { fetchedUser } from "@/types/Interfaces"
 import { $authHost, $host } from "./hosts"
 
+type searchedUser = {
+    id: number
+}
+
 export const getById = async (id: number): Promise<fetchedUser> => {
     const response = await $host.get(`/users/${id}`)
     return response.data
@@ -9,5 +13,10 @@ export const getById = async (id: number): Promise<fetchedUser> => {
 export const editUserBio = async (dto: any) => {
     const response = await $authHost.post('users/update', dto)
     console.log(response);
+    return response.data
+}
+
+export const searchUser = async (query: string): Promise<searchedUser> => {
+    const response = await $host.get(`users/search/${query}`)
     return response.data
 }
