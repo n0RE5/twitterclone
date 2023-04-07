7a621172ef0b1e9d0936b86294890ca37809c010
n0RE <nore@desktop>
2023-04-07 21:23:46 +0300

    added fetchFollowing functions

--- twitter-clone/httpAPI/followAPI.ts
+++ twitter-clone/httpAPI/followAPI.ts
@@ -1,5 +1,13 @@
 import { CreateFollowDto } from "@/types/dto"
 import { $authHost, $host } from "./hosts"
+import { getById } from "./userAPI"
+import { fetchedUser } from "@/types/Interfaces"
+
+interface user_subs {
+    id: number,
+    userId: number
+    subscriptionId: number
+}
 
 export const followUser = async (dto: CreateFollowDto): Promise<CreateFollowDto> => {
     const response = await $authHost.post('subscriptions/subscribe', dto)
@@ -24,4 +32,19 @@ export const countFollowers = async (userId: number): Promise<number> => {
 export const countFollowing = async (userId: number): Promise<number> => {
     const response = await $host.get(`subscriptions/following/${userId}`)
     return response.data
+}
+
+const getUserArray = async (res: user_subs[]) => {
+    const users = []
+    for (let element of res) {
+        const user = await getById(element.userId)
+        users.push(user)
+    }
+    return users
+}
+
+export const getUserFollows = async(): Promise<fetchedUser[]> => {
+    const response = await $authHost.get('subscriptions')
+    const users = await getUserArray(response.data)
+    return users
 }
